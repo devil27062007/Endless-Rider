6163cd827b3171768c8d22b9e16ad2c99debc688
smoker27007 <roshini.g010@gmail.com>
2026-04-02 12:43:34 +0530

    player movement added

--- src/character.js
+++ src/character.js
@@ -1,4 +1,4 @@
-import { ctx } from "./main.js";
+import { ctx ,canvas } from "./main.js";
 
 export const playerSpriteSheet = new Image();
 playerSpriteSheet.src = "../assets/bk_cars1.png";
@@ -6,9 +6,16 @@ playerSpriteSheet.src = "../assets/bk_cars1.png";
 export const player = {
     x: 0,
     y: 0,
-    speed: 0,
+    speed: 1,
     isDead: false,
     nitro: 0
+}
+
+export const keys = {
+    up:false ,
+    down: false,
+    right: false ,
+    down: false
 }
 
 export const playerSprite = { x: 297, y: 347, w: 39, h: 83 };
@@ -17,14 +24,47 @@ let lastTime = 0;
 
 
 export function updatePplayer() {
+    let moveY = 0;
+    let moveX = 0;
 
+    if(keys.up){
+        moveY -= 1;
+    }
+    if(keys.down){
+        moveY += 1;
+    }
+    if(keys.right){
+        moveX += 1;
+    }
+    if(keys.left){
+        moveX -=1;
+    }
+    if(keys.right && keys.up ){
+        moveX *= Math.SQRT2 /2;
+        moveY *= Math.SQRT2 / 2;
+    }
+    if(keys.left && keys.up){
+        moveX *= Math.SQRT2 / 2 ;
+        moveY *= Math.SQRT2 / 2;
+    }
+    if(keys.right && keys.down){
+        moveX *= Math.SQRT2 / 2 ;
+        moveY *= Math.SQRT2 / 2 ;
+    }
+    if(keys.left && keys.down){
+        moveX *= Math.SQRT2 / 2 ;
+        moveY *= Math.SQRT2 / 2 ;
+    }
+
+    player.x += moveX* player.speed ;
+    player.y += moveY * player.speed;
 };
 
 export function drawPlayer() {
     ctx.drawImage(
         playerSpriteSheet,
         playerSprite.x, playerSprite.y, playerSprite.w, playerSprite.h,
-        0, 0, playerSprite.w, playerSprite.h
+        player.x, player.y , playerSprite.w, playerSprite.h
     );
 }
 
@@ -35,5 +75,8 @@ export function gameLoop(currentTime) {
     if (delta > 0.1) delta = 0.1;
 
     lastTime = currentTime;
+    updatePplayer();
+    drawPlayer();
 
+    requestAnimationFrame(gameLoop);
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { drawPlayer, player, playerSprite, playerSpriteSheet } from "./character.js";
+import { drawPlayer,gameLoop, keys, player, playerSprite, playerSpriteSheet } from "./character.js";
 
 export const canvas = document.getElementById("game-canvas");
 export const ctx = canvas.getContext("2d");
@@ -15,3 +15,52 @@ window.addEventListener("resize", () => {
     resizeCanvas();
 })
 resizeCanvas();
+
+function imageLoaded(){
+    let imageCount = 1 ;
+    let loadedCount = 0;
+
+    if(playerSpriteSheet.onload){
+        loadedCount +=1;
+    }
+
+    return imageCount === loadedCount ;
+}
+
+function startGame(){
+    const check = imageLoaded();
+    if(check){
+        requestAnimationFrame(gameLoop);
+    } else {
+        console.log("game assets no yet loaded");
+    }
+}
+
+document.addEventListener('keydown',(e) =>{
+    console.log(e.key);
+    switch(e.key.toLowerCase()){
+        case 'w':
+        case 'arrowup':keys.up = true ; return;
+        case 'a':
+        case 'arrowleft': keys.left = true ; return ;
+        case 's':
+        case 'arrowdown':keys.down = true ; return ;
+        case 'd':
+        case'arrowright':keys.down = true ; return
+    }
+});
+
+document.addEventListener('keyup',(e)=>{
+    switch(e.key.toLowerCase()){
+        case 'w':
+        case 'arrowup': keys.up = false ; return ;
+        case 'a':
+        case 'arrowup': keys.left = false ; return ;
+        case 's':
+        case 'arrowdown': keys.down = false ; return ;
+        case 'd':
+        case 'arrowright': keys.right = false; return;
+    }
+});
+
+startGame();
